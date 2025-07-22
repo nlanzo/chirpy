c8960e22a3864a137d532807b1c436191d49ca7a
Nick Lanzo <nlanzo@gmail.com>
2025-07-22 12:51:48 -0500

    add optional paramaters to GET /api/chirps to allow for filtering by user or sorting ascending/descending by created_at

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,17 +2,35 @@ package main
 
 import (
 	"net/http"
+	"sort"
 
 	"github.com/google/uuid"
+	"github.com/nlanzo/chirpy/internal/database"
 )
 
 func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
+	var chirps []database.Chirp
 	response := []Chirp{}
-
-	chirps, err := cfg.db.GetAllChirps(r.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Failed to get chirps", err)
-		return
+	err := error(nil)
+	
+	userID := r.URL.Query().Get("author_id")
+	if userID == "" {
+		chirps, err = cfg.db.GetAllChirps(r.Context())
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Failed to get chirps", err)
+			return
+		}
+	} else {
+		userID, err := uuid.Parse(userID)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid user ID", err)
+			return
+		}
+		chirps, err = cfg.db.GetChirpsByUserID(r.Context(), userID)
+		if err != nil {
+			respondWithError(w, http.StatusNotFound, "Chirps not found", err)
+			return
+		}
 	}
 
 	for _, chirp := range chirps {
@@ -24,6 +42,16 @@ func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
 			UserId:    chirp.UserID,
 		})
 	}
+
+	// response is default sorted by created_at ascending
+	// if sort is "desc", sort by created_at descending
+	sortDirection := r.URL.Query().Get("sort")
+	if sortDirection == "desc" {
+		sort.Slice(response, func(i, j int) bool {
+			return response[i].CreatedAt.After(response[j].CreatedAt)
+		})
+	}
+
 	respondWithJSON(w, http.StatusOK, response)
 }
 
